25219f6ee4da157b5994551885b5a2f86bd733e8
QuintonStudent <49251721+QuintonStudent@users.noreply.github.com>
2020-01-24 01:04:40 -0500

    rock paper scissors finally working recursively

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -3,8 +3,29 @@
 import sys
 
 def rock_paper_scissors(n):
-  pass 
+  # declare variables
+  shoot = ['rock', 'paper', 'scissors']
+  result = []
+  temp = []
 
+  # create recursive function
+  def rps_recursive(x, temp=[]):
+
+  # create base case
+    if x == 0:
+      return result.append(temp)
+  
+  # loop through rock, paper, scissors list
+    for index in shoot:
+      
+  # call recursive function
+      rps_recursive(x - 1, temp + [index])
+
+  # call recursive function again
+  rps_recursive(n)
+
+  # return result
+  return result
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
